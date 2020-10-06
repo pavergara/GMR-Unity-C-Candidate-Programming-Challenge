a85b7c9baaf56f6e6614087a078f7a23bbc305be
pavergara <pato.vergara@gmail.com>
2020-10-06 12:01:25 -0300

    Version inicial. Solo fuentes.
    Version incial para evaluacion de GMR  en proceso de seleccion

---
+++ GMR Candidate Programming Challenge/Assets/Scripts/JsonFileManagerSingleton.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class JsonFileManagerSingleton : MonoBehaviour
+{
+
+    public static JsonFileManagerSingleton _Instance { get; private set; }
+
+    public string _filename = "JsonList.json";//"JsonChallenge.json";
+
+
+    List<string> campos = new List<string>();
+
+
+    private string jsonString; //String Json loaded from file.
+    private JObject JParsed; //Parser Json
+
+    const string JSON_TITLE = "Title";
+    const string JSON_HEADERS = "ColumnHeaders";
+    const string JSON_DATA = "Data";
+
+    private string _title;
+    private string[] _columns;
+    private List<List<string>> _data;
+
+    public string Title { get { return (JParsed==null)?null:_title; } }
+    public string[] Columns { get { return (JParsed == null) ? null : _columns; } }
+    public List<List<string>> Data { get { return (JParsed == null) ? null : _data; } }
+
+
+    private void Awake()
+    {
+        if (_Instance == null)
+        {
+            _Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ReadData()
+    {
+
+        //erase all data loaded previously
+
+        JParsed = null;
+
+        _title = null;
+        _columns = null;
+        _data = null;
+
+
+        if (ValidateFileExist())
+        {
+            LoadJsonFile();
+        }
+        else
+        {
+            Debug.Log("File not Exist: \""+ _filename + "\"");
+            
+        }
+    }
+
+
+    public bool ValidateFileExist()
+    {
+        try
+        {
+            return VerifyExistFile(_filename);
+        }
+        catch
+        {
+            
+            return false;
+        }
+    }
+
+
+    private bool VerifyExistFile(string filename)
+    {
+
+        string fullFileName = Path.Combine(Application.streamingAssetsPath, filename);
+        print(fullFileName);
+
+        if (File.Exists(fullFileName))
+        {
+            Debug.Log("Existe JSON Para Leer, " + fullFileName);
+            return true;
+        }
+
+        throw new Exception("Json file not found", new IOException());
+
+
+    }
+
+    //read JSON file defined by name, in StreamingAssets path. Result iun global String for parsed after.
+    private void JsonBaseRead()
+    {
+        //Read file
+        jsonString = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, _filename));
+
+        Debug.Log("antes de..." + jsonString);
+
+
+    }
+
+    private void LoadJsonFile()
+    {
+        
+        jsonString = "";
+
+        JsonBaseRead(); //Load jsonString
+
+        if (jsonString.Equals(""))
+        {
+            print("Json read empty");
+            return;
+        }
+
+        JParsed = null;
+
+        JParsed = JObject.Parse(jsonString);
+
+
+        //Title
+        try
+        {
+            Debug.Log(" JParsed.Property(" + JSON_TITLE +"):" + JParsed.Property(JSON_TITLE).Value);
+            _title = JParsed.Property(JSON_TITLE).Value.ToString();
+        }
+        catch
+        {
+
+            Debug.Log("Error to access property \""+ JSON_TITLE + "\" (maybe empty ?)");
+            _title = null;
+        }
+
+        //ColumnHeaders
+        try
+        {
+            Debug.Log(" JParsed.Property(" + JSON_HEADERS+"):" + (JArray)JParsed.Property(JSON_HEADERS).Value);
+
+            string[] _out = new string[((JArray)(JParsed.Property("ColumnHeaders").Value)).Count];
+
+            for (int i = 0; i < ((JArray)(JParsed.Property("ColumnHeaders").Value)).Count; i++) //bjr.ColumnHeaders)
+            {
+                _out[i] = (string)((JArray)(JParsed.Property("ColumnHeaders").Value))[i].Value<string>();
+            }
+            Debug.Log("return from Json Nsoft");
+            _columns = _out;
+
+
+        }
+        catch
+        {
+            Debug.Log("Error to access property \""+JSON_HEADERS+"\" (maybe empty ?)");
+            _columns = null;
+        }
+
+        //Data
+        try
+        {
+            Debug.Log(" JParsed.Property(" + JSON_DATA+")[0]:" + ((JArray)JParsed.Property(JSON_DATA).Value)[0]);
+
+            List<List<string>> lista = new List<List<string>>();
+
+            foreach (JObject jo in ((JArray)(JParsed.Property("Data").Value)))
+            {
+                List<string> values = new List<string>();
+                foreach (string s in ((JArray)(JParsed.Property("ColumnHeaders").Value))) //bjr.ColumnHeaders)
+                {
+                    try
+                    {
+                        values.Add((string)jo.Property(s).Value);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Log(e.Message + "" + e.StackTrace);
+                        values.Add("");
+                    } //Dejar columna en blanco si es que no viene en seccion DATA json.
+                }
+
+                lista.Add(values);
+            }
+
+            _data = lista;
+
+        }
+        catch
+        {
+            Debug.Log("Error to access property \""+ JSON_DATA+"[0]\" (maybe empty ?)");
+            _data = null;
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
---
+++ GMR Candidate Programming Challenge/Assets/Scripts/LoadUIController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class LoadUIController : MonoBehaviour
+{
+
+
+    public Text TextfieldRef;
+
+    public Text UITextTitle;
+
+    public Transform HeaderTransform, DataTransform;
+
+    public JsonFileManagerSingleton RefJsonFileLoader;
+
+
+
+    //private string title;
+    //private string[] HeadersColumns;
+    //private List<List<string>> data;
+
+    private int HeadersColumnsCount = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadUIDataFronJson();
+    }
+
+
+    public void LoadUIDataFronJson()
+    {
+        if (!RefJsonFileLoader)
+        {
+            print("Dont have any Json Reference");
+            return;
+        }
+
+        //Leee Json y almacena en la clase. Sin este llamado, por defeto no se carga el Json.
+        RefJsonFileLoader.ReadData();
+
+        //Post read data, load on UI
+
+        LoadTitle(RefJsonFileLoader.Title ?? "No data read from Title");
+
+        LoadHeaders(RefJsonFileLoader.Columns ?? (new string[] { "No data read from Columns " }));
+        //fix to length of headers
+        //HeadersColumns = RefJsonFileLoader.Columns;
+        HeadersColumnsCount = (RefJsonFileLoader.Columns==null)?0: RefJsonFileLoader.Columns.Length;
+
+
+        LoadData(RefJsonFileLoader.Data ?? new List<List<string>> { new List<string> { "No data read from Data" } } );
+    }
+
+
+
+
+    void LoadTitle(string titleText)
+    {
+
+        //Text UITextTitle = transform.GetChild(0).gameObject.GetComponent<Text>();
+
+        UITextTitle.text = titleText;
+    }
+
+
+    void LoadHeaders(string[] titles)
+    {
+        //Transform HeaderTransform = transform.GetChild(1).GetChild(0).transform;
+
+        //string[] titles = jr.GetColumnTitles();
+
+        //ColumnHeaders
+
+        Text CellTemp;
+
+        foreach (string s in titles)
+        {
+            CellTemp = Instantiate<Text>(TextfieldRef, HeaderTransform);
+            CellTemp.GetComponent<Text>().text = s;
+            CellTemp.fontStyle = FontStyle.Bold;
+            CellTemp.gameObject.SetActive(true);
+        }
+        //
+        HeaderTransform.gameObject.GetComponent<GridLayoutGroup>().constraintCount = titles.Length;
+        HeaderTransform.gameObject.GetComponent<GridLayoutGroup>().childAlignment = TextAnchor.UpperLeft;
+        
+    }
+
+    void LoadData(List<List<string>> values)
+    {
+        
+
+        Text CellTemp;
+
+        //Data
+        foreach (List<string> ls in values)
+        {
+            foreach (string s in ls)
+            {
+                CellTemp = Instantiate<Text>(TextfieldRef, DataTransform);
+                CellTemp.GetComponent<Text>().text = s;
+                CellTemp.fontStyle = FontStyle.Normal;
+                CellTemp.gameObject.SetActive(true);
+
+            }
+        }
+        //
+        DataTransform.gameObject.GetComponent<GridLayoutGroup>().constraintCount = HeadersColumnsCount;
+        DataTransform.gameObject.GetComponent<GridLayoutGroup>().childAlignment = TextAnchor.UpperLeft;
+        
+    }
+
+
+    public void CleanContent()
+    {
+        //Title
+        UITextTitle.text = "New Text";
+
+        //Headers
+        foreach(Transform t in HeaderTransform)
+        {
+            Destroy(t.gameObject);
+        }
+
+        //Data
+        foreach (Transform t in DataTransform)
+        {
+            Destroy(t.gameObject);
+        }
+
+
+    }
+
+    public void ReloadDataoFromJson()
+    {
+        CleanContent();
+        LoadUIDataFronJson();
+    }
+}
